df2d0bcb1959499618ae71ac4d2f1f51fce1d6be
jui <juideshmukh07@email.com>
2026-04-03 15:38:33 +0530

    implement LoanContract with fund/repay/claim state machine and inner transactions

--- smart_contracts/loan_contract/deploy_config.py
+++ smart_contracts/loan_contract/deploy_config.py
@@ -1,44 +1,52 @@
 import logging
-
+import base64
+import json
+import algosdk
+from algosdk.transaction import ApplicationCreateTxn, OnComplete, StateSchema
 import algokit_utils
 
 logger = logging.getLogger(__name__)
 
-
-# define deployment behaviour based on supplied app spec
 def deploy() -> None:
-    from smart_contracts.artifacts.hello_world.hello_world_client import (
-        HelloArgs,
-        HelloWorldFactory,
-    )
-
     algorand = algokit_utils.AlgorandClient.from_environment()
     deployer_ = algorand.account.from_environment("DEPLOYER")
 
-    factory = algorand.client.get_typed_app_factory(
-        HelloWorldFactory, default_sender=deployer_.address
+    with open("smart_contracts/artifacts/loan_contract/LoanContract.arc56.json") as f:
+        spec = json.load(f)
+
+    # Use pre-compiled bytecode from ARC56
+    approval = base64.b64decode(spec["byteCode"]["approval"])
+    clear = base64.b64decode(spec["byteCode"]["clear"])
+
+    method = algosdk.abi.Method.from_signature("create_loan(uint64,uint64,uint64,uint64)void")
+    selector = method.get_selector()
+
+    app_call = ApplicationCreateTxn(
+        sender=deployer_.address,
+        sp=algorand.client.algod.suggested_params(),
+        on_complete=OnComplete.NoOpOC,
+        approval_program=approval,
+        clear_program=clear,
+        global_schema=StateSchema(
+            spec["state"]["schema"]["global"]["ints"],
+            spec["state"]["schema"]["global"]["bytes"],
+        ),
+        local_schema=StateSchema(
+            spec["state"]["schema"]["local"]["ints"], spec["state"]["schema"]["local"]["bytes"]
+        ),
+        app_args=[
+            selector,
+            (1000000).to_bytes(8, "big"),
+            (30).to_bytes(8, "big"),
+            (1).to_bytes(8, "big"),
+            (0).to_bytes(8, "big"),
+        ],
     )
 
-    app_client, result = factory.deploy(
-        on_update=algokit_utils.OnUpdate.AppendApp,
-        on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
-    )
+    signed_txn = app_call.sign(deployer_.private_key)
+    txid = algorand.client.algod.send_transaction(signed_txn)
+    result = algosdk.transaction.wait_for_confirmation(algorand.client.algod, txid, 4)
+    app_id = result["application-index"]
 
-    if result.operation_performed in [
-        algokit_utils.OperationPerformed.Create,
-        algokit_utils.OperationPerformed.Replace,
-    ]:
-        algorand.send.payment(
-            algokit_utils.PaymentParams(
-                amount=algokit_utils.AlgoAmount(algo=1),
-                sender=deployer_.address,
-                receiver=app_client.app_address,
-            )
-        )
-
-    name = "world"
-    response = app_client.send.hello(args=HelloArgs(name=name))
-    logger.info(
-        f"Called hello on {app_client.app_name} ({app_client.app_id}) "
-        f"with name={name}, received: {response.abi_return}"
-    )
+    logger.info(f"Deployed LoanContract! APP_ID: {app_id}")
+    print(f"\nAPP_ID_IS_HERE={app_id}\n")
